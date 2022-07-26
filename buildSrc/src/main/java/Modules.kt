ba3ba55d6eb394f1ad61059dc4f74ca8e51785f4
sammy-mutahi <sammymutahi21@gmail.com>
2022-07-26 17:10:14 +0300

    * add data layer dependencies

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,4 +1,4 @@
 object Modules {
-    const val presentation = ":forecast:forecast_presentation"
-    const val domain = ":forecast:forecast_domain"
+    const val presentation = ":sell:sell_presentation"
+    const val domain = ":sell:sell_domain"
 }
