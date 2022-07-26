a566beee3b3e38585357d00b23e8f8c66a8f351e
sammy-mutahi <sammymutahi21@gmail.com>
2022-07-27 01:01:52 +0300

    add bottom navigation

--- sell/sell_presentation/src/main/java/com/sammy/sell_presentation/HomeFragment.kt
+++ sell/sell_presentation/src/main/java/com/sammy/sell_presentation/HomeFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.sammy.sell_presentation.databinding.FragmentHomeBinding
+import com.sammy.sell_presentation.ui.BottomNavigationFragment
 import com.sammy.sell_presentation.ui.DashboardViewModel
+import com.sammy.sell_presentation.ui.MenuNavigationItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : BottomNavigationFragment() {
 
     private val homeBinding: FragmentHomeBinding by lazy {
         FragmentHomeBinding.inflate(layoutInflater)
@@ -25,6 +27,26 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return homeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        homeBinding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    openScreen(MenuNavigationItem.HOME)
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "Feature Coming Soon !!", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+            true
+        }
     }
 
 }
---
+++ sell/sell_presentation/src/main/java/com/sammy/sell_presentation/ui/MenuNavigationItem.kt
@@ -0,0 +1,7 @@
+package com.sammy.sell_presentation.ui
+
+import com.sammy.sell_presentation.R
+
+enum class MenuNavigationItem(val tag: String, val menuId: Int) {
+    HOME("home", R.id.nav_home)
+}
